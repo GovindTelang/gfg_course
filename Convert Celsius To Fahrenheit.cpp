c0c44284e1e4cb64f15a6990cca7f1d86e0b7b04
Govind Telang <133373913+GovindTelang@users.noreply.github.com>
2025-01-29 19:49:55 +0530

    Create Convert Celsius To Fahrenheit.cpp

---
+++ Convert Celsius To Fahrenheit.cpp
@@ -0,0 +1,11 @@
+//Given a temperature in celsius C. You need to convert the given temperature to Fahrenheit.
+class Solution{
+    public:
+    
+    //You need to complete this function
+    double cToF(int C)
+    {
+        return (1.8*C+32);
+        //Your code here
+    }
+};
