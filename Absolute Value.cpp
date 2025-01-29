5126ca035ad6c8b97c0347a960efec0669041c74
Govind Telang <133373913+GovindTelang@users.noreply.github.com>
2025-01-29 19:48:55 +0530

    Create Absolute Value.cpp

---
+++ Absolute Value.cpp
@@ -0,0 +1,11 @@
+//You are given an interger I, find the absolute value of the interger I.
+
+class Solution{
+    public:
+        int absolute(int I) {
+        // Your code goes here
+        if(I>=0)  return I;
+        return (-1)*I;
+    }
+
+};
